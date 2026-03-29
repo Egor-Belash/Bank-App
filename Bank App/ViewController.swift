64d089098070a696ddb8a5e4f00b59f349f4f8fd
Egor <egorbla228@gmail.com>
2026-03-29 12:59:13 +0300

    Initial Commit

---
+++ Bank App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank App
+//
+//  Created by Egor on 29.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
